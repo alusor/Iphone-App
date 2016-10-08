a9104a07c357a0dec83e8a059e45669bf8f5c71c
Eduardo Velez <Alusor@MacBook-Pro-de-Eduardo.local>
2016-10-08 11:29:56 -0500

    Initial Commit

---
+++ crud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  crud
+//
+//  Created by Alusor on 08/10/16.
+//  Copyright © 2016 Alusor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  crud
+//
+//  Created by Alusor on 08/10/16.
+//  Copyright © 2016 Alusor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
